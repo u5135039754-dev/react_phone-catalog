776b80032549b769628de7433f86844450243e24
Your Name <u513509754@gmail.com>
2026-05-18 06:14:48 -0700

    deploy: latest production build to gh-pages

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  base: command === 'build' ? '/react_phone-catalog/' : '/',
-}))
